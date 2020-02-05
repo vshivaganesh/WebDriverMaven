3d75d772eb69ab7c2ced1ea452c64f74354fdbc7
Shiva <vshivaganesh@gmail.com>
2020-02-04 22:49:23 -0500

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void doLogin() {
+
+		driver.get("https://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("vshivaganesh@gmail.com");
+		driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+		driver.findElement(By.name("password")).sendKeys("ghesvsdr123");
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		driver.quit();
+	}
+
+}
